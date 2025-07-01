a4c27a1c13ffbba23282e046e4dc8ea5553e4f4d
Eduardo Bartikoski <eduardob.i207@gmail.com>
2025-07-01 12:41:47 -0300

    att 3

--- public/script.js
+++ public/script.js
@@ -1,5 +1,15 @@
-async function buscarMensagem() {
-    const resposta = await fetch('api/mensagem');
-    const dados = await resposta.json();
-    document.getElementById('resultado').textContent = dados.mensagem;
-}
+document.getElementById('resultado').addEventListener('click',async() => {
+    try {
+        const resposta = await fetch('api/mensagem');
+        const dados = await resposta.json();
+        console.log(dados.mensagem);
+    } catch (erro) {
+        console.error('Erro ao buscar mensgem:  ', erro);
+    }
+})
+
+
+
+
+
+    
--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'public','index.html'));
 });
 
+app.get('/api/mensagem', (req,res) => {
+    res.json({mensagem: 'Olá do servidor'});
+});
+
 //iniciando servidor
 app.listen(3000, ()=> {
     console.log('Servidor rodando em http://localhost:3000');
