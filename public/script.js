d16511ed203491f17d657803fe9ad0cad6cb2e18
Eduardo Bartikoski <eduardob.i207@gmail.com>
2025-07-01 12:50:46 -0300

    up final

--- public/script.js
+++ public/script.js
@@ -1,15 +1,17 @@
-document.getElementById('resultado').addEventListener('click',async() => {
-    try {
-        const resposta = await fetch('api/mensagem');
-        const dados = await resposta.json();
-        console.log(dados.mensagem);
-    } catch (erro) {
-        console.error('Erro ao buscar mensgem:  ', erro);
-    }
-})
+document.addEventListener('DOMContentLoaded', () => {
+    const botao = document.getElementById('btnBuscar');
 
+    botao.addEventListener('click', async () => {
+        try {
+            const resposta = await fetch('/api/mensagem');
+            const dados = await resposta.json();
+            console.log(dados.mensagem);
 
-
-
-
-    
+            // Mostrar no HTML também
+            document.getElementById('resultado').textContent = dados.mensagem;
+        } catch (erro) {
+            console.error('Erro ao buscar mensagem:', erro);
+            document.getElementById('resultado').textContent = 'Erro na requisição';
+        }
+    });
+});
