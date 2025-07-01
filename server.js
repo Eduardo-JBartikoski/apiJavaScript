699894ea52ab0934789abd0c3dbc7087626afb46
Eduardo Bartikoski <eduardob.i207@gmail.com>
2025-07-01 12:30:19 -0300

    att2

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const app = express();
 
 app.use(cors()); // uso html
 app.use(express.json()); // leitura JSON requisição
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //get
-app.get('/api/mensagem', (req, res) => {
-    res.json({mensagem: 'Olá do servidor' });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'public','index.html'));
 });
 
 //iniciando servidor
